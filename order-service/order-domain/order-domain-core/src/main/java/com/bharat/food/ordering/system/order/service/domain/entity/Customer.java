32424cc4164d2a91098b36a1682a9090376bfe57
Bharat Verma <bindian0509@gmail.com>
2022-08-20 23:17:52 +0530

    Adding state changing methods to Order Entity

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/bharat/food/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.bharat.food.ordering.system.order.service.domain.entity;
+
+import com.bharat.food.ordering.system.domain.entity.AggregateRoot;
+import com.bharat.food.ordering.system.domain.vo.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
--- order-service/order-domain/order-domain-core/src/main/java/com/bharat/food/ordering/system/order/service/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/bharat/food/ordering/system/order/service/domain/entity/Order.java
@@ -11,6 +11,7 @@ import com.bharat.food.ordering.system.order.service.domain.vo.OrderItemId;
 import com.bharat.food.ordering.system.order.service.domain.vo.StreetAdress;
 import com.bharat.food.ordering.system.order.service.domain.vo.TrackingId;
 
+import javax.swing.plaf.BorderUIResource;
 import java.util.List;
 import java.util.UUID;
 
@@ -39,6 +40,44 @@ public class Order extends AggregateRoot<OrderId> {
         validateItemsPrice();
     }
 
+    public void pay() {
+        if (orderStatus != OrderStatus.PENDING) {
+            throw new OrderDomainException("Order is not in correct state for payment operation!");
+        }
+        orderStatus = OrderStatus.PAID;
+    }
+
+    public void approve() {
+        if(orderStatus != OrderStatus.PAID) {
+            throw new OrderDomainException("Order is not in correct state for approve operation!");
+        }
+        orderStatus = OrderStatus.APPROVED;
+    }
+
+    public void initCancel(List<String> failureMessages) {
+        if(orderStatus != OrderStatus.PAID) {
+            throw new OrderDomainException("Order is not in correct state for init cancel operation!");
+        }
+        orderStatus = OrderStatus.CANCELLING;
+        updateFailureMessages(failureMessages);
+    }
+
+    public void cancel(List<String> failureMessages) {
+        if(!(orderStatus == OrderStatus.CANCELLING || orderStatus == OrderStatus.PENDING)) {
+            throw new OrderDomainException("Order is not in correct state for cancel operation!");
+        }
+        orderStatus = OrderStatus.CANCELLED;
+        updateFailureMessages(failureMessages);
+    }
+
+    private void updateFailureMessages(List<String> failureMessages) {
+        if(this.failureMessages != null && failureMessages != null) {
+            this.failureMessages.addAll(failureMessages.stream().filter(message -> !message.isEmpty()).toList());
+        }
+        if(this.failureMessages == null) {
+            this.failureMessages = failureMessages;
+        }
+    }
     private void validateItemsPrice() {
         Money orderItemsTotal = items.stream().map(orderItem -> {
            validateItemPrice(orderItem);
