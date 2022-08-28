a8b5e103afb23bc94b619ae0dc64bdbb728452ba
Bharat Verma <bindian0509@gmail.com>
2022-08-28 23:27:40 +0530

    Adding Order Event and Domain Service

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/bharat/food/ordering/system/order/service/domain/OrderDomainService.java
@@ -0,0 +1,23 @@
+package com.bharat.food.ordering.system.order.service.domain;
+
+import com.bharat.food.ordering.system.order.service.domain.entity.Order;
+import com.bharat.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.bharat.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.bharat.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.bharat.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+
+import java.util.List;
+
+public interface OrderDomainService {
+
+    OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant);
+
+    OrderPaidEvent PayOrder(Order order);
+
+    void approveOrder();
+
+    OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages);
+
+    void cancelOrder(Order order, List<String> failureMessages);
+
+}
--- order-service/order-domain/order-domain-core/src/main/java/com/bharat/food/ordering/system/order/service/domain/event/OrderCreatedEvent.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/bharat/food/ordering/system/order/service/domain/event/OrderCreatedEvent.java
@@ -1,25 +1,12 @@
 package com.bharat.food.ordering.system.order.service.domain.event;
 
-import com.bharat.food.ordering.system.domain.event.DomainEvent;
 import com.bharat.food.ordering.system.order.service.domain.entity.Order;
 
 import java.time.ZonedDateTime;
 
-public class OrderCreatedEvent implements DomainEvent<Order> {
-
-    private final Order order;
-    private final ZonedDateTime createdAt;
+public class OrderCreatedEvent  extends OrderEvent {
 
     public OrderCreatedEvent(Order order, ZonedDateTime createdAt) {
-        this.order = order;
-        this.createdAt = createdAt;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public ZonedDateTime getCreatedAt() {
-        return createdAt;
+        super(order, createdAt);
     }
 }
