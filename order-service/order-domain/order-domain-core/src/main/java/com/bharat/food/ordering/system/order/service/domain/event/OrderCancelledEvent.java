782aa160a5a8bd3074222026f6e66df547a3c510
Bharat Verma <bindian0509@gmail.com>
2022-08-28 23:04:15 +0530

    Adding order event different states

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/bharat/food/ordering/system/order/service/domain/event/OrderCancelledEvent.java
@@ -0,0 +1,25 @@
+package com.bharat.food.ordering.system.order.service.domain.event;
+
+
+import com.bharat.food.ordering.system.domain.event.DomainEvent;
+import com.bharat.food.ordering.system.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCancelledEvent implements DomainEvent<Order> {
+    private final Order order;
+    private final ZonedDateTime createdAt;
+
+    public OrderCancelledEvent(Order order, ZonedDateTime createdAt) {
+        this.order = order;
+        this.createdAt = createdAt;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/bharat/food/ordering/system/order/service/domain/event/OrderPaidEvent.java
@@ -0,0 +1,24 @@
+package com.bharat.food.ordering.system.order.service.domain.event;
+
+import com.bharat.food.ordering.system.domain.event.DomainEvent;
+import com.bharat.food.ordering.system.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderPaidEvent implements DomainEvent<Order> {
+    private final Order order;
+    private final ZonedDateTime createdAt;
+
+    public OrderPaidEvent(Order order, ZonedDateTime createdAt) {
+        this.order = order;
+        this.createdAt = createdAt;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
