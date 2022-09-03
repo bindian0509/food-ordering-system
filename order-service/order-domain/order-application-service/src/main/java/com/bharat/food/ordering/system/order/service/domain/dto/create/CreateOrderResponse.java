4225e05903fbbce186df8a20414e6c9f997e71b2
Bharat Verma <bindian0509@gmail.com>
2022-09-03 15:55:43 +0530

    Update CreateOrderResponse.java

--- order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
+++ order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
@@ -1,4 +1,18 @@
 package com.bharat.food.ordering.system.order.service.domain.dto.create;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class CreateOrderResponse {
+
+
+    private final UUID orderTrackingId;
+    private final
+    private final 
 }
