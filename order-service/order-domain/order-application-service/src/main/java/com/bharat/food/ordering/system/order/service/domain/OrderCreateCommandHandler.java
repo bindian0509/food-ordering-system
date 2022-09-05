a1d344cee678eff8bcc2f07e6670aa27ad5a78c2
Bharat Verma <bindian0509@gmail.com>
2022-09-05 23:49:26 +0530

    Publish with transactional listener

--- order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -27,15 +27,20 @@ public class OrderCreateCommandHandler {
     private final CustomerRepository customerRepository;
     private final RestaurantRepository restaurantRepository;
     private final OrderDataMapper orderDataMapper;
+    private final ApplicationDomainEventPublisher applicationDomainEventPublisher;
 
-    public OrderCreateCommandHandler(OrderDomainService orderDomainService, OrderRepository orderRepository,
-                                     CustomerRepository customerRepository, RestaurantRepository restaurantRepository,
-                                     OrderDataMapper orderDataMapper) {
+    public OrderCreateCommandHandler(OrderDomainService orderDomainService,
+                                     OrderRepository orderRepository,
+                                     CustomerRepository customerRepository,
+                                     RestaurantRepository restaurantRepository,
+                                     OrderDataMapper orderDataMapper,
+                                     ApplicationDomainEventPublisher applicationDomainEventPublisher) {
         this.orderDomainService = orderDomainService;
         this.orderRepository = orderRepository;
         this.customerRepository = customerRepository;
         this.restaurantRepository = restaurantRepository;
         this.orderDataMapper = orderDataMapper;
+        this.applicationDomainEventPublisher = applicationDomainEventPublisher;
     }
 
     @Transactional
@@ -46,6 +51,7 @@ public class OrderCreateCommandHandler {
         OrderCreatedEvent orderCreatedEvent = orderDomainService.validateAndInitiateOrder(order, restaurant);
         Order orderResult = saveOrder(order);
         log.info("Order is created with Order Id : {} ", orderResult.getId().getValue());
+        applicationDomainEventPublisher.publish(orderCreatedEvent);
         return orderDataMapper.orderToCreateOrderResponse(orderResult, "Order is created with Order Id : "+orderResult.getId().getValue());
     }
 
---
+++ order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/OrderCreatedEventApplicationListener.java
@@ -0,0 +1,25 @@
+package com.bharat.food.ordering.system.order.service.domain;
+
+import com.bharat.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.bharat.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Slf4j
+@Component
+public class OrderCreatedEventApplicationListener {
+
+
+    private final OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher;
+
+    public OrderCreatedEventApplicationListener(OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher) {
+        this.orderCreatedPaymentRequestMessagePublisher = orderCreatedPaymentRequestMessagePublisher;
+    }
+
+    @TransactionalEventListener
+    void process (OrderCreatedEvent orderCreatedEvent) {
+
+
+    }
+}
