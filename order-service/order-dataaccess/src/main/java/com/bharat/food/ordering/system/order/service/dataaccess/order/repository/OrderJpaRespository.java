7b9f20034d4277724898ca61d1349efaccf58c98
Bharat Verma <bindian0509@gmail.com>
2022-09-30 23:52:12 +0530

    Data access module: Adding JPA Repository & Adapter implementation

---
+++ order-service/order-dataaccess/src/main/java/com/bharat/food/ordering/system/order/service/dataaccess/order/repository/OrderJpaRespository.java
@@ -0,0 +1,14 @@
+package com.bharat.food.ordering.system.order.service.dataaccess.order.repository;
+
+import com.bharat.food.ordering.system.order.service.dataaccess.order.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface OrderJpaRespository extends JpaRepository<OrderEntity, UUID> {
+
+    Optional<OrderEntity> findByTrackingId (UUID trackingId);
+}
