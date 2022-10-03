fe33fc576bb56e5a230c7db56f9d0becd826d495
Bharat Verma <bindian0509@gmail.com>
2022-10-04 00:11:16 +0530

    Data access module: Adding Customer and Restaurant implementations II

--- order-service/order-dataaccess/src/main/java/com/bharat/food/ordering/system/order/service/dataaccess/restaurant/entity/RestaurantEntity.java
+++ order-service/order-dataaccess/src/main/java/com/bharat/food/ordering/system/order/service/dataaccess/restaurant/entity/RestaurantEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -24,9 +25,9 @@ public class RestaurantEntity {
     private UUID productId;
 
     private String restaurantName;
-    private String restaurantActive;
+    private Boolean restaurantActive;
     private String productName;
-    private String productPrice;
+    private BigDecimal productPrice;
 
     @Override
     public boolean equals(Object o) {
--- order-service/order-dataaccess/src/main/java/com/bharat/food/ordering/system/order/service/dataaccess/restaurant/repository/RestaurantJpaRespository.java
+++ order-service/order-dataaccess/src/main/java/com/bharat/food/ordering/system/order/service/dataaccess/restaurant/repository/RestaurantJpaRespository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
