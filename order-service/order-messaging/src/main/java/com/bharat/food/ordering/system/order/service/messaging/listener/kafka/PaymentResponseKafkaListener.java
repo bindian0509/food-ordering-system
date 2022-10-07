3a3167ec0e608ac716527b4c333691e7b5ad90f9
Bharat Verma <bindian0509@gmail.com>
2022-10-07 23:56:55 +0530

    Messaging module: Adding Listener implementations 2

--- order-service/order-messaging/src/main/java/com/bharat/food/ordering/system/order/service/messaging/listener/kafka/PaymentResponseKafkaListener.java
+++ order-service/order-messaging/src/main/java/com/bharat/food/ordering/system/order/service/messaging/listener/kafka/PaymentResponseKafkaListener.java
@@ -2,12 +2,37 @@ package com.bharat.food.ordering.system.order.service.messaging.listener.kafka;
 
 import com.bharat.food.ordering.system.kafka.consumer.KafkaConsumer;
 import com.bharat.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
+import com.bharat.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
+import com.bharat.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
+@Slf4j
 public class PaymentResponseKafkaListener implements KafkaConsumer<PaymentResponseAvroModel> {
+
+    private final PaymentResponseMessageListener paymentResponseMessageListener;
+    private final OrderMessagingDataMapper orderMessagingDataMapper;
+
+
+    public PaymentResponseKafkaListener(PaymentResponseMessageListener paymentResponseMessageListener,
+                                        OrderMessagingDataMapper orderMessagingDataMapper) {
+        this.paymentResponseMessageListener = paymentResponseMessageListener;
+        this.orderMessagingDataMapper = orderMessagingDataMapper;
+    }
+
     @Override
-    public void receive(List<PaymentResponseAvroModel> messages, List<Long> keys, List<Integer> partitions, List<Long> offsets) {
+    @KafkaListener(id = "${kafka-consumer-config.payment-consumer-group-id}", topics = "${order-service.payment-response-topic-name}")
+    public void receive(@Payload  List<PaymentResponseAvroModel> messages,
+                        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) List<Long> keys,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
+                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
 
     }
 }
