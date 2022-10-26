31b8910d12e5eb25bd7bb76fc92b83ecaced7c4f
Bharat Verma <bindian0509@gmail.com>
2022-10-26 22:23:04 +0530

    Added customer service module

---
+++ customer-service/src/main/java/com/bharat/food/ordering/system/customer/service/CustomerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bharat.food.ordering.system.customer.service;
+/*
+ * @author bharat.verma
+ * @created Wednesday, 26 October 2022
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+}
