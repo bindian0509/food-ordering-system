c217b3532b568ac1000d973e0a4e4414ec15b09e
Bharat Verma <bindian0509@gmail.com>
2022-10-27 17:52:23 +0530

    Added main class and application yml for customer service

--- customer-service/src/main/java/com/bharat/food/ordering/system/customer/service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/bharat/food/ordering/system/customer/service/CustomerServiceApplication.java
@@ -4,9 +4,13 @@ package com.bharat.food.ordering.system.customer.service;
  * @created Wednesday, 26 October 2022
  */
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bharat.food.ordering.system")
 public class CustomerServiceApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
 }
