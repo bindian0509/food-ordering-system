2f262b355c839e02c0051136982503603cf008f2
Bharat Verma <bindian0509@gmail.com>
2022-10-30 12:21:12 +0530

    running project corrections - 2

--- order-service/order-messaging/src/main/java/com/bharat/food/ordering/system/order/service/messaging/listener/kafka/RestaurantApprovalResponseKafkaListener.java
+++ order-service/order-messaging/src/main/java/com/bharat/food/ordering/system/order/service/messaging/listener/kafka/RestaurantApprovalResponseKafkaListener.java
@@ -16,48 +16,49 @@ import java.util.List;
 
 import static com.bharat.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
 
-@Component
+
 @Slf4j
+@Component
 public class RestaurantApprovalResponseKafkaListener implements KafkaConsumer<RestaurantApprovalResponseAvroModel> {
 
     private final RestaurantApprovalResponseMessageListener restaurantApprovalResponseMessageListener;
     private final OrderMessagingDataMapper orderMessagingDataMapper;
 
-    public RestaurantApprovalResponseKafkaListener(
-            RestaurantApprovalResponseMessageListener restaurantApprovalResponseMessageListener,
-            OrderMessagingDataMapper orderMessagingDataMapper) {
+    public RestaurantApprovalResponseKafkaListener(RestaurantApprovalResponseMessageListener
+                                                           restaurantApprovalResponseMessageListener,
+                                                   OrderMessagingDataMapper orderMessagingDataMapper) {
         this.restaurantApprovalResponseMessageListener = restaurantApprovalResponseMessageListener;
         this.orderMessagingDataMapper = orderMessagingDataMapper;
     }
 
     @Override
-    @KafkaListener(id = "${kafka-consumer-config.restaurant-approval-consumer-group-id",
-    topics = "${restaurant-service.restaurant-approval-response-topic-name}")
+    @KafkaListener(id = "${kafka-consumer-config.restaurant-approval-consumer-group-id}",
+            topics = "${order-service.restaurant-approval-response-topic-name}")
     public void receive(@Payload List<RestaurantApprovalResponseAvroModel> messages,
                         @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) List<Long> keys,
                         @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
                         @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
-
-
-
         log.info("{} number of restaurant approval responses received with keys {}, partitions {} and offsets {}" ,
                 messages.size(),
                 partitions.toString(),
                 keys.toString(),
                 offsets.toString());
+
         messages.forEach(restaurantApprovalResponseAvroModel -> {
-            if(OrderApprovalStatus.APPROVED == restaurantApprovalResponseAvroModel.getOrderApprovalStatus()) {
+            if (OrderApprovalStatus.APPROVED == restaurantApprovalResponseAvroModel.getOrderApprovalStatus()) {
                 log.info("Processing approved order for order id: {}",
                         restaurantApprovalResponseAvroModel.getOrderId());
                 restaurantApprovalResponseMessageListener.orderApproved(orderMessagingDataMapper
-                        .approvalResponseAvroModelToApprovalResponse(restaurantApprovalResponseAvroModel)) ;
+                        .approvalResponseAvroModelToApprovalResponse(restaurantApprovalResponseAvroModel));
             } else if (OrderApprovalStatus.REJECTED == restaurantApprovalResponseAvroModel.getOrderApprovalStatus()) {
                 log.info("Processing rejected order for order id: {}, with failure messages: {}",
-                        restaurantApprovalResponseAvroModel.getOrderId(), String.join(FAILURE_MESSAGE_DELIMITER,
+                        restaurantApprovalResponseAvroModel.getOrderId(),
+                        String.join(FAILURE_MESSAGE_DELIMITER,
                                 restaurantApprovalResponseAvroModel.getFailureMessages()));
                 restaurantApprovalResponseMessageListener.orderRejected(orderMessagingDataMapper
                         .approvalResponseAvroModelToApprovalResponse(restaurantApprovalResponseAvroModel));
             }
         });
+
     }
 }
