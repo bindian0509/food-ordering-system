2e56cb0d2b08da82b79ace38ac2b9356df62e3b8
Bharat Verma <bindian0509@gmail.com>
2022-11-07 12:21:12 +0530

    CreditEntry Entity initialized

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/entity/CreditEntry.java
@@ -0,0 +1,12 @@
+package com.bharat.food.ordering.system.payment.service.domain.entity;
+/*
+ * @author bharat.verma
+ * @created Friday, 20 January 2023
+ */
+
+import com.bharat.food.ordering.system.domain.entity.BaseEntity;
+import com.bharat.food.ordering.system.payment.service.domain.valueobject.CreditEntryId;
+
+public class CreditEntry extends BaseEntity<CreditEntryId> {
+
+}
---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/valueobject/CreditEntryId.java
@@ -0,0 +1,8 @@
+package com.bharat.food.ordering.system.payment.service.domain.valueobject;
+/*
+ * @author bharat.verma
+ * @created Friday, 20 January 2023
+ */
+
+public class CreditEntryId {
+}
