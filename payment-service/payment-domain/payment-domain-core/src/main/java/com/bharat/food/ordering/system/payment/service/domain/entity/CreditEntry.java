2c6a724754e32c3ddf5386a8936acece6ea3377a
Bharat Verma <bindian0509@gmail.com>
2022-11-09 12:21:12 +0530

    CreditEntry Completed

--- payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/entity/CreditEntry.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/entity/CreditEntry.java
@@ -5,8 +5,66 @@ package com.bharat.food.ordering.system.payment.service.domain.entity;
  */
 
 import com.bharat.food.ordering.system.domain.entity.BaseEntity;
+import com.bharat.food.ordering.system.domain.vo.CustomerId;
+import com.bharat.food.ordering.system.domain.vo.Money;
 import com.bharat.food.ordering.system.payment.service.domain.valueobject.CreditEntryId;
 
 public class CreditEntry extends BaseEntity<CreditEntryId> {
 
+    private final CustomerId customerId;
+    private Money totalCreditAmount;
+
+    public void addCreditAmount(Money amount) {
+        this.totalCreditAmount.add(amount);
+    }
+
+    public void subtractCreditAmount(Money amount) {
+        this.totalCreditAmount.substract(amount);
+    }
+
+    private CreditEntry(Builder builder) {
+        setId(builder.creditEntryId);
+        customerId = builder.customerId;
+        totalCreditAmount = builder.totalCreditAmount;
+    }
+
+    public CustomerId getCustomerId() {
+        return customerId;
+    }
+
+    public Money getTotalCreditAmount() {
+        return totalCreditAmount;
+    }
+
+    public static final class Builder {
+        private CreditEntryId creditEntryId;
+        private CustomerId customerId;
+        private Money totalCreditAmount;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder creditEntryId(CreditEntryId val) {
+            creditEntryId = val;
+            return this;
+        }
+
+        public Builder customerId(CustomerId val) {
+            customerId = val;
+            return this;
+        }
+
+        public Builder totalCreditAmount(Money val) {
+            totalCreditAmount = val;
+            return this;
+        }
+
+        public CreditEntry build() {
+            return new CreditEntry(this);
+        }
+    }
 }
