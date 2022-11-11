50cd2fa0d5d90dd52762bf58ff896192b02b6a1b
Bharat Verma <bindian0509@gmail.com>
2022-11-11 12:21:12 +0530

    CreditHistory Entity completed

--- payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/entity/CreditHistory.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/entity/CreditHistory.java
@@ -5,7 +5,71 @@ package com.bharat.food.ordering.system.payment.service.domain.entity;
  */
 
 import com.bharat.food.ordering.system.domain.entity.BaseEntity;
+import com.bharat.food.ordering.system.domain.vo.CustomerId;
+import com.bharat.food.ordering.system.domain.vo.Money;
 import com.bharat.food.ordering.system.payment.service.domain.valueobject.CreditHistoryId;
+import com.bharat.food.ordering.system.payment.service.domain.valueobject.TransactionType;
 
 public class CreditHistory extends BaseEntity<CreditHistoryId> {
+
+    private final CustomerId customerId;
+    private final Money amount;
+    private final TransactionType transactionType;
+
+    private CreditHistory(Builder builder) {
+        setId(builder.creditHistoryId);
+        customerId = builder.customerId;
+        amount = builder.amount;
+        transactionType = builder.transactionType;
+    }
+
+    public CustomerId getCustomerId() {
+        return customerId;
+    }
+
+    public Money getAmount() {
+        return amount;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public static final class Builder {
+        private CreditHistoryId creditHistoryId;
+        private CustomerId customerId;
+        private Money amount;
+        private TransactionType transactionType;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder creditHistoryId(CreditHistoryId val) {
+            creditHistoryId = val;
+            return this;
+        }
+
+        public Builder customerId(CustomerId val) {
+            customerId = val;
+            return this;
+        }
+
+        public Builder amount(Money val) {
+            amount = val;
+            return this;
+        }
+
+        public Builder transactionType(TransactionType val) {
+            transactionType = val;
+            return this;
+        }
+
+        public CreditHistory build() {
+            return new CreditHistory(this);
+        }
+    }
 }
---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/valueobject/TransactionType.java
@@ -0,0 +1,5 @@
+package com.bharat.food.ordering.system.payment.service.domain.valueobject;
+
+public enum TransactionType {
+    DEBIT, CREDIT
+}
