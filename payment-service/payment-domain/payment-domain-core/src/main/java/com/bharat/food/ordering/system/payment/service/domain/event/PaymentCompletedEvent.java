e48d7ef22215b46bb6cfbd6dd1feae1b84ee18e4
Bharat Verma <bindian0509@gmail.com>
2022-11-13 12:21:12 +0530

    Payment Events Added

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/event/PaymentCompletedEvent.java
@@ -0,0 +1,16 @@
+package com.bharat.food.ordering.system.payment.service.domain.event;
+/*
+ * @author bharat.verma
+ * @created Friday, 20 January 2023
+ */
+
+import com.bharat.food.ordering.system.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class PaymentCompletedEvent extends PaymentEvent {
+    public PaymentCompletedEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
+        super(payment, createdAt, failureMessages);
+    }
+}
---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/bharat/food/ordering/system/payment/service/domain/event/PaymentEvent.java
@@ -0,0 +1,36 @@
+package com.bharat.food.ordering.system.payment.service.domain.event;
+/*
+ * @author bharat.verma
+ * @created Friday, 20 January 2023
+ */
+
+import com.bharat.food.ordering.system.domain.event.DomainEvent;
+import com.bharat.food.ordering.system.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public abstract class PaymentEvent implements DomainEvent<Payment> {
+
+    private final Payment payment;
+    private final ZonedDateTime createdAt;
+    private final List<String> failureMessages;
+
+    public PaymentEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
+        this.payment = payment;
+        this.createdAt = createdAt;
+        this.failureMessages = failureMessages;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public List<String> getFailureMessages() {
+        return failureMessages;
+    }
+}
