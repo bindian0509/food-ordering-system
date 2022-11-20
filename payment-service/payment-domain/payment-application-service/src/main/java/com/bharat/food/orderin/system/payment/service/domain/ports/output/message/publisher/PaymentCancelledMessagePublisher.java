bdae98ffd39c22c6b0bd6d0de7b45f07747acc4e
Bharat Verma <bindian0509@gmail.com>
2022-11-20 20:21:12 +0530

    payment application service WIP IV

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/bharat/food/orderin/system/payment/service/domain/ports/output/message/publisher/PaymentCancelledMessagePublisher.java
@@ -0,0 +1,12 @@
+package com.bharat.food.orderin.system.payment.service.domain.ports.output.message.publisher;
+/*
+ * @author bharat.verma
+ * @created Monday, 23 January 2023
+ */
+
+import com.bharat.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.bharat.food.ordering.system.payment.service.domain.event.PaymentCancelledEvent;
+
+public interface PaymentCancelledMessagePublisher extends DomainEventPublisher<PaymentCancelledEvent> {
+
+}
