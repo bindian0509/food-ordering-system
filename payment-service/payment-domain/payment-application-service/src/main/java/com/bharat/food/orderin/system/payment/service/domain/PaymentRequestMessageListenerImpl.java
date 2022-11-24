9eda7248f2f565343586c6a8ef2ea08ff1c45626
Bharat Verma <bindian0509@gmail.com>
2022-11-24 23:11:22 +0530

    Application Service domain module: Implementing input ports - Part 2

--- payment-service/payment-domain/payment-application-service/src/main/java/com/bharat/food/orderin/system/payment/service/domain/PaymentRequestMessageListenerImpl.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/bharat/food/orderin/system/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -6,19 +6,60 @@ package com.bharat.food.orderin.system.payment.service.domain;
 
 import com.bharat.food.orderin.system.payment.service.domain.dto.PaymentRequest;
 import com.bharat.food.orderin.system.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
+import com.bharat.food.orderin.system.payment.service.domain.ports.output.message.publisher.PaymentCancelledMessagePublisher;
+import com.bharat.food.orderin.system.payment.service.domain.ports.output.message.publisher.PaymentCompletedMessagePublisher;
+import com.bharat.food.orderin.system.payment.service.domain.ports.output.message.publisher.PaymentFailedMessagePublisher;
+import com.bharat.food.ordering.system.payment.service.domain.entity.Payment;
+import com.bharat.food.ordering.system.payment.service.domain.event.PaymentCancelledEvent;
+import com.bharat.food.ordering.system.payment.service.domain.event.PaymentCompletedEvent;
+import com.bharat.food.ordering.system.payment.service.domain.event.PaymentEvent;
+import com.bharat.food.ordering.system.payment.service.domain.event.PaymentFailedEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageListener {
+
+    private final PaymentRequestHelper paymentRequestHelper;
+    private final PaymentCompletedMessagePublisher paymentCompletedMessagePublisher;
+    private final PaymentCancelledMessagePublisher paymentCancelledMessagePublisher;
+    private final PaymentFailedMessagePublisher paymentFailedMessagePublisher;
+
+    public PaymentRequestMessageListenerImpl(PaymentRequestHelper paymentRequestHelper,
+                                             PaymentCompletedMessagePublisher paymentCompletedMessagePublisher,
+                                             PaymentCancelledMessagePublisher paymentCancelledMessagePublisher,
+                                             PaymentFailedMessagePublisher paymentFailedMessagePublisher) {
+        this.paymentRequestHelper = paymentRequestHelper;
+        this.paymentCompletedMessagePublisher = paymentCompletedMessagePublisher;
+        this.paymentCancelledMessagePublisher = paymentCancelledMessagePublisher;
+        this.paymentFailedMessagePublisher = paymentFailedMessagePublisher;
+    }
+
     @Override
     public void completePayment(PaymentRequest paymentRequest) {
+        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequest);
+        fireEvent(paymentEvent);
 
     }
 
     @Override
     public void cancelPayment(PaymentRequest paymentRequest) {
+        PaymentEvent paymentEvent = paymentRequestHelper.persistCancelPayment(paymentRequest);
+        fireEvent(paymentEvent);
+    }
 
+    private void fireEvent(PaymentEvent paymentEvent) {
+        log.info("Publishing payment event with payment id: {} and order id: {}",
+                paymentEvent.getPayment().getId().getValue(),
+                paymentEvent.getPayment().getOrderId().getValue());
+        if (paymentEvent instanceof PaymentCompletedEvent) {
+            paymentCompletedMessagePublisher.publish((PaymentCompletedEvent) paymentEvent);
+        } else if (paymentEvent instanceof PaymentCancelledEvent) {
+            paymentCancelledMessagePublisher.publish((PaymentCancelledEvent) paymentEvent);
+        } else if (paymentEvent instanceof PaymentFailedEvent) {
+            paymentFailedMessagePublisher.publish((PaymentFailedEvent) paymentEvent);
+        }
     }
 }
