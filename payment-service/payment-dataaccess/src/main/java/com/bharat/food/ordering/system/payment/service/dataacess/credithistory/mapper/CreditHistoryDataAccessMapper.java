ac3758cb6bc7adc0a5a60d8e052f5151ff97836f
Bharat Verma <bindian0509@gmail.com>
2022-12-02 01:02:30 +0530

    Data Access : CreditHistoryEntry Entity Completed

---
+++ payment-service/payment-dataaccess/src/main/java/com/bharat/food/ordering/system/payment/service/dataacess/credithistory/mapper/CreditHistoryDataAccessMapper.java
@@ -0,0 +1,35 @@
+package com.bharat.food.ordering.system.payment.service.dataacess.credithistory.mapper;
+/*
+ * @author bharat.verma
+ * @created Thursday, 26 January 2023
+ */
+
+import com.bharat.food.ordering.system.domain.vo.CustomerId;
+import com.bharat.food.ordering.system.domain.vo.Money;
+import com.bharat.food.ordering.system.payment.service.dataacess.credithistory.entity.CreditHistoryEntity;
+import com.bharat.food.ordering.system.payment.service.domain.entity.CreditHistory;
+import com.bharat.food.ordering.system.payment.service.domain.valueobject.CreditHistoryId;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreditHistoryDataAccessMapper {
+
+    public CreditHistory creditHistoryEntityToCreditHistory(CreditHistoryEntity creditHistoryEntity) {
+        return CreditHistory.builder()
+                .creditHistoryId(new CreditHistoryId(creditHistoryEntity.getId()))
+                .customerId(new CustomerId(creditHistoryEntity.getCustomerId()))
+                .amount(new Money(creditHistoryEntity.getAmount()))
+                .transactionType(creditHistoryEntity.getType())
+                .build();
+    }
+
+    public CreditHistoryEntity creditHistoryToCreditHistoryEntity(CreditHistory creditHistory) {
+        return CreditHistoryEntity.builder()
+                .id(creditHistory.getId().getValue())
+                .customerId(creditHistory.getCustomerId().getValue())
+                .amount(creditHistory.getAmount().getAmount())
+                .type(creditHistory.getTransactionType())
+                .build();
+    }
+
+}
