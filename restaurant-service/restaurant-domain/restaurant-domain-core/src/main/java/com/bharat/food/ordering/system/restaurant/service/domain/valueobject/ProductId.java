3b193dabd13d93f151a7867df4c6521c8e232e9f
Bharat Verma <bindian0509@gmail.com>
2022-12-19 08:08:08 +0530

    Product Entity

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/valueobject/ProductId.java
@@ -0,0 +1,16 @@
+package com.bharat.food.ordering.system.restaurant.service.domain.valueobject;
+/*
+ * @author bharat.verma
+ * @created Sunday, 29 January 2023
+ */
+
+import com.bharat.food.ordering.system.domain.vo.BaseId;
+
+import java.util.UUID;
+
+public class ProductId extends BaseId<UUID> {
+
+    protected ProductId(UUID value) {
+        super(value);
+    }
+}
