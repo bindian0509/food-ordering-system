0175b825029a185a23729dcb331f33600995eaf5
Bharat Verma <bindian0509@gmail.com>
2022-12-22 20:02:22 +0530

    Added Events 1

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/event/OrderRejectedEvent.java
@@ -0,0 +1,8 @@
+package com.bharat.food.ordering.system.restaurant.service.domain.event;
+/*
+ * @author bharat.verma
+ * @created Monday, 06 February 2023
+ */
+
+public class OrderRejectedEvent {
+}
