d1414f71a5d189e0e8d292c71b0cb5a59f9b2fb7
Bharat Verma <bindian0509@gmail.com>
2022-12-23 20:02:22 +0530

    Added Events 2

--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/event/OrderRejectedEvent.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/event/OrderRejectedEvent.java
@@ -4,5 +4,29 @@ package com.bharat.food.ordering.system.restaurant.service.domain.event;
  * @created Monday, 06 February 2023
  */
 
-public class OrderRejectedEvent {
+
+import com.bharat.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.bharat.food.ordering.system.domain.vo.RestaurantId;
+import com.bharat.food.ordering.system.restaurant.service.domain.entity.OrderApproval;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OrderRejectedEvent extends OrderApprovalEvent {
+
+    private final DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher;
+
+    public OrderRejectedEvent(OrderApproval orderApproval,
+                              RestaurantId restaurantId,
+                              List<String> failureMessages,
+                              ZonedDateTime createdAt,
+                              DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher) {
+        super(orderApproval, restaurantId, failureMessages, createdAt);
+        this.orderRejectedEventDomainEventPublisher = orderRejectedEventDomainEventPublisher;
+    }
+
+    @Override
+    public void fire() {
+        orderRejectedEventDomainEventPublisher.publish(this);
+    }
 }
