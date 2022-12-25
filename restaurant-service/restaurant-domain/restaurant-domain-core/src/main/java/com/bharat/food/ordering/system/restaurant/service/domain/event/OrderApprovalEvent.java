8b1df1b198768137ecc80226202bdc02b449d67c
Bharat Verma <bindian0509@gmail.com>
2022-12-25 20:02:22 +0530

    Added Events 4

--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/event/OrderApprovalEvent.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/event/OrderApprovalEvent.java
@@ -4,5 +4,42 @@ package com.bharat.food.ordering.system.restaurant.service.domain.event;
  * @created Monday, 06 February 2023
  */
 
-public class OrderApprovalEvent {
+import com.bharat.food.ordering.system.domain.event.DomainEvent;
+import com.bharat.food.ordering.system.domain.vo.RestaurantId;
+import com.bharat.food.ordering.system.restaurant.service.domain.entity.OrderApproval;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public abstract class OrderApprovalEvent implements DomainEvent<OrderApproval> {
+    private final OrderApproval orderApproval;
+    private final RestaurantId restaurantId;
+    private final List<String> failureMessages;
+    private final ZonedDateTime createdAt;
+
+    public OrderApprovalEvent(OrderApproval orderApproval,
+                              RestaurantId restaurantId,
+                              List<String> failureMessages,
+                              ZonedDateTime createdAt) {
+        this.orderApproval = orderApproval;
+        this.restaurantId = restaurantId;
+        this.failureMessages = failureMessages;
+        this.createdAt = createdAt;
+    }
+
+    public OrderApproval getOrderApproval() {
+        return orderApproval;
+    }
+
+    public RestaurantId getRestaurantId() {
+        return restaurantId;
+    }
+
+    public List<String> getFailureMessages() {
+        return failureMessages;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
