7514130a39180a0ca38f092feac1501a8d1152cd
Bharat Verma <bindian0509@gmail.com>
2023-01-01 20:02:22 +0530

    Restaurant Application Service V

---
+++ common/common-domain/src/main/java/com/bharat/food/ordering/system/domain/vo/RestaurantOrderStatus.java
@@ -0,0 +1,8 @@
+package com.bharat.food.ordering.system.domain.vo;
+/*
+ * @author bharat.verma
+ * @created Tuesday, 07 February 2023
+ */
+public enum RestaurantOrderStatus {
+    PAID
+}
--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/dto/RestaurantApprovalRequest.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/dto/RestaurantApprovalRequest.java
@@ -4,6 +4,7 @@ package com.bharat.food.ordering.system.restaurant.service.domain.dto;
  * @created Tuesday, 07 February 2023
  */
 
+import com.bharat.food.ordering.system.domain.vo.RestaurantOrderStatus;
 import com.bharat.food.ordering.system.restaurant.service.domain.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
