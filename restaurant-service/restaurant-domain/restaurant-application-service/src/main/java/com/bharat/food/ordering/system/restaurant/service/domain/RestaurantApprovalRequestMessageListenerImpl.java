3db2bcdff97e55e9a2c0102228b0c9637b395e38
Bharat Verma <bindian0509@gmail.com>
2023-01-05 20:02:22 +0530

    Restaurant Application Service IX

---
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/RestaurantApprovalRequestMessageListenerImpl.java
@@ -0,0 +1,30 @@
+package com.bharat.food.ordering.system.restaurant.service.domain;
+/*
+ * @author bharat.verma
+ * @created Tuesday, 07 February 2023
+ */
+
+import com.bharat.food.ordering.system.restaurant.service.domain.dto.RestaurantApprovalRequest;
+import com.bharat.food.ordering.system.restaurant.service.domain.event.OrderApprovalEvent;
+import com.bharat.food.ordering.system.restaurant.service.domain.ports.input.message.listener.RestaurantApprovalRequestMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RestaurantApprovalRequestMessageListenerImpl implements RestaurantApprovalRequestMessageListener {
+
+    private final RestaurantApprovalRequestHelper restaurantApprovalRequestHelper;
+
+    public RestaurantApprovalRequestMessageListenerImpl(RestaurantApprovalRequestHelper
+                                                                restaurantApprovalRequestHelper) {
+        this.restaurantApprovalRequestHelper = restaurantApprovalRequestHelper;
+    }
+
+    @Override
+    public void approveOrder(RestaurantApprovalRequest restaurantApprovalRequest) {
+        OrderApprovalEvent orderApprovalEvent =
+                restaurantApprovalRequestHelper.persistOrderApproval(restaurantApprovalRequest);
+        orderApprovalEvent.fire();
+    }
+}
