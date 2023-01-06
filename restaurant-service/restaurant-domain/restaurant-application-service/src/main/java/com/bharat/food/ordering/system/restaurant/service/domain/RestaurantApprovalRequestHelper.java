21d04e39c9a322ccd4e747d30bbb66debe9c8f29
Bharat Verma <bindian0509@gmail.com>
2023-01-06 20:02:22 +0530

    Restaurant Application Service X

---
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/bharat/food/ordering/system/restaurant/service/domain/RestaurantApprovalRequestHelper.java
@@ -0,0 +1,89 @@
+package com.bharat.food.ordering.system.restaurant.service.domain;
+/*
+ * @author bharat.verma
+ * @created Tuesday, 07 February 2023
+ */
+
+
+import com.bharat.food.ordering.system.domain.vo.OrderId;
+import com.bharat.food.ordering.system.restaurant.service.domain.dto.RestaurantApprovalRequest;
+import com.bharat.food.ordering.system.restaurant.service.domain.entity.Restaurant;
+import com.bharat.food.ordering.system.restaurant.service.domain.event.OrderApprovalEvent;
+import com.bharat.food.ordering.system.restaurant.service.domain.exception.RestaurantNotFoundException;
+import com.bharat.food.ordering.system.restaurant.service.domain.mapper.RestaurantDataMapper;
+import com.bharat.food.ordering.system.restaurant.service.domain.ports.output.message.publisher.OrderApprovedMessagePublisher;
+import com.bharat.food.ordering.system.restaurant.service.domain.ports.output.message.publisher.OrderRejectedMessagePublisher;
+import com.bharat.food.ordering.system.restaurant.service.domain.ports.output.repository.OrderApprovalRepository;
+import com.bharat.food.ordering.system.restaurant.service.domain.ports.output.repository.RestaurantRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class RestaurantApprovalRequestHelper {
+
+    private final RestaurantDomainService restaurantDomainService;
+    private final RestaurantDataMapper restaurantDataMapper;
+    private final RestaurantRepository restaurantRepository;
+    private final OrderApprovalRepository orderApprovalRepository;
+    private final OrderApprovedMessagePublisher orderApprovedMessagePublisher;
+    private final OrderRejectedMessagePublisher orderRejectedMessagePublisher;
+
+    public RestaurantApprovalRequestHelper(RestaurantDomainService restaurantDomainService,
+                                           RestaurantDataMapper restaurantDataMapper,
+                                           RestaurantRepository restaurantRepository,
+                                           OrderApprovalRepository orderApprovalRepository,
+                                           OrderApprovedMessagePublisher orderApprovedMessagePublisher,
+                                           OrderRejectedMessagePublisher orderRejectedMessagePublisher) {
+        this.restaurantDomainService = restaurantDomainService;
+        this.restaurantDataMapper = restaurantDataMapper;
+        this.restaurantRepository = restaurantRepository;
+        this.orderApprovalRepository = orderApprovalRepository;
+        this.orderApprovedMessagePublisher = orderApprovedMessagePublisher;
+        this.orderRejectedMessagePublisher = orderRejectedMessagePublisher;
+    }
+
+    @Transactional
+    public OrderApprovalEvent persistOrderApproval(RestaurantApprovalRequest restaurantApprovalRequest) {
+        log.info("Processing restaurant approval for order id: {}", restaurantApprovalRequest.getOrderId());
+        List<String> failureMessages = new ArrayList<>();
+        Restaurant restaurant = findRestaurant(restaurantApprovalRequest);
+        OrderApprovalEvent orderApprovalEvent =
+                restaurantDomainService.validateOrder(
+                        restaurant,
+                        failureMessages,
+                        orderApprovedMessagePublisher,
+                        orderRejectedMessagePublisher);
+        orderApprovalRepository.save(restaurant.getOrderApproval());
+        return orderApprovalEvent;
+    }
+
+    private Restaurant findRestaurant(RestaurantApprovalRequest restaurantApprovalRequest) {
+        Restaurant restaurant = restaurantDataMapper
+                .restaurantApprovalRequestToRestaurant(restaurantApprovalRequest);
+        Optional<Restaurant> restaurantResult = restaurantRepository.findRestaurantInformation(restaurant);
+        if (restaurantResult.isEmpty()) {
+            log.error("Restaurant with id " + restaurant.getId().getValue() + " not found!");
+            throw new RestaurantNotFoundException("Restaurant with id " + restaurant.getId().getValue() +
+                    " not found!");
+        }
+
+        Restaurant restaurantEntity = restaurantResult.get();
+        restaurant.setActive(restaurantEntity.isActive());
+        restaurant.getOrderDetail().getProducts().forEach(product ->
+                restaurantEntity.getOrderDetail().getProducts().forEach(p -> {
+                    if (p.getId().equals(product.getId())) {
+                        product.updateWithConfirmedNamePriceAndAvailability(p.getName(), p.getPrice(), p.isAvailable());
+                    }
+                }));
+        restaurant.getOrderDetail().setId(new OrderId(UUID.fromString(restaurantApprovalRequest.getOrderId())));
+
+        return restaurant;
+    }
+}
