a2d1adfc52713f1dfd825d5b6c6634bad4dc2b09
Bharat Verma <bindian0509@gmail.com>
2023-02-08 11:03:43 +0530

    Implementing Order Payment Saga I

---
+++ infrastructure/saga/src/main/java/com/bharat/food/ordering/system/saga/SagaStep.java
@@ -0,0 +1,8 @@
+package com.bharat.food.ordering.system.saga;
+
+import com.bharat.food.ordering.system.domain.event.DomainEvent;
+
+public interface SagaStep <T, S extends DomainEvent, U extends DomainEvent> {
+    S process (T data);
+    U rollback (T data);
+}
