f4d44388000e43318872d72e349ac262119214a5
Bharat Verma <bindian0509@gmail.com>
2023-02-09 15:15:58 +0530

    Using Order Payment Saga in Messaging & Implementing Order Approval Saga

---
+++ order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/OrderApprovalSaga.java
@@ -0,0 +1,56 @@
+package com.bharat.food.ordering.system.order.service.domain;
+/*
+ * @author bharat.verma
+ * @created Thursday, 09 February 2023
+ */
+
+import com.bharat.food.ordering.system.domain.event.EmptyEvent;
+import com.bharat.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import com.bharat.food.ordering.system.order.service.domain.entity.Order;
+import com.bharat.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.bharat.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCancelledPaymentRequestMessagePublisher;
+import com.bharat.food.ordering.system.saga.SagaStep;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+public class OrderApprovalSaga implements SagaStep<RestaurantApprovalResponse, EmptyEvent, OrderCancelledEvent> {
+
+    private final OrderDomainService orderDomainService;
+    private final OrderSagaHelper orderSagaHelper;
+    private final OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher;
+
+    public OrderApprovalSaga(OrderDomainService orderDomainService,
+                             OrderSagaHelper orderSagaHelper,
+                             OrderCancelledPaymentRequestMessagePublisher
+                                     orderCancelledPaymentRequestMessagePublisher) {
+        this.orderDomainService = orderDomainService;
+        this.orderSagaHelper = orderSagaHelper;
+        this.orderCancelledPaymentRequestMessagePublisher = orderCancelledPaymentRequestMessagePublisher;
+    }
+    @Override
+    @Transactional
+    public EmptyEvent process(RestaurantApprovalResponse restaurantApprovalResponse) {
+        log.info("Approving order with id: {}", restaurantApprovalResponse.getOrderId());
+        Order order = orderSagaHelper.findOrder(restaurantApprovalResponse.getOrderId());
+        orderDomainService.approveOrder(order);
+        orderSagaHelper.saveOrder(order);
+        log.info("Order with id: {} is approved", order.getId().getValue());
+        return EmptyEvent.INSTANCE;
+    }
+
+    @Override
+    @Transactional
+    public OrderCancelledEvent rollback(RestaurantApprovalResponse restaurantApprovalResponse) {
+        log.info("Cancelling order with id: {}", restaurantApprovalResponse.getOrderId());
+        Order order = orderSagaHelper.findOrder(restaurantApprovalResponse.getOrderId());
+        OrderCancelledEvent domainEvent = orderDomainService.cancelOrderPayment(order,
+                restaurantApprovalResponse.getFailureMessages(),
+                orderCancelledPaymentRequestMessagePublisher);
+        orderSagaHelper.saveOrder(order);
+        log.info("Order with id: {} is cancelling", order.getId().getValue());
+        return domainEvent;
+    }
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/OrderSagaHelper.java
@@ -0,0 +1,40 @@
+package com.bharat.food.ordering.system.order.service.domain;
+/*
+ * @author bharat.verma
+ * @created Thursday, 09 February 2023
+ */
+
+
+import com.bharat.food.ordering.system.domain.vo.OrderId;
+import com.bharat.food.ordering.system.order.service.domain.entity.Order;
+import com.bharat.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.bharat.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class OrderSagaHelper {
+
+    private final OrderRepository orderRepository;
+
+    public OrderSagaHelper(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    Order findOrder(String orderId) {
+        Optional<Order> orderResponse = orderRepository.findById(new OrderId(UUID.fromString(orderId)));
+        if (orderResponse.isEmpty()) {
+            log.error("Order with id: {} could not be found!", orderId);
+            throw new OrderNotFoundException("Order with id " + orderId + " could not be found!");
+        }
+        return orderResponse.get();
+    }
+
+    void saveOrder(Order order) {
+        orderRepository.save(order);
+    }
+}
