1818cf1772d59f6a5eed5455c2f158b8ff205a20
Bharat Verma <bindian0509@gmail.com>
2023-02-09 10:54:21 +0530

    Payment Response Message Listener

--- order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -1,22 +1,41 @@
 package com.bharat.food.ordering.system.order.service.domain;
 
 import com.bharat.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.bharat.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import com.bharat.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import static com.bharat.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
+
 @Slf4j
 @Service
 @Validated
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+
+
+    private final OrderPaymentSaga orderPaymentSaga;
+
+    public PaymentResponseMessageListenerImpl(OrderPaymentSaga orderPaymentSaga) {
+        this.orderPaymentSaga = orderPaymentSaga;
+    }
+
     @Override
     public void paymentCompleted(PaymentResponse paymentResponse) {
+
+        OrderPaidEvent domainEvent = orderPaymentSaga.process(paymentResponse);
+        log.info("Publishing orderPaidEvent for orderId : {} ", paymentResponse.getOrderId());
+        domainEvent.fire();
 
     }
 
     @Override
     public void paymentCancelled(PaymentResponse paymentResponse) {
 
+        orderPaymentSaga.rollback(paymentResponse);
+        log.info("order is rolled back for orderId : {} with following failure messages : {} ",
+                paymentResponse.getOrderId(),
+                String.join(FAILURE_MESSAGE_DELIMITER, paymentResponse.getFailureMessages()));
     }
 }
