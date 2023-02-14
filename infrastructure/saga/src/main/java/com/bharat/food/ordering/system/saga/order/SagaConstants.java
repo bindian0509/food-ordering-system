dbd8de32531754033ffe6172b7705c5996ab03cd
Bharat Verma <bindian0509@gmail.com>
2023-02-14 23:42:30 +0530

    Refactoring Order domain layer: Adding Outbox scheduler

---
+++ infrastructure/saga/src/main/java/com/bharat/food/ordering/system/saga/order/SagaConstants.java
@@ -0,0 +1,11 @@
+package com.bharat.food.ordering.system.saga.order;
+/*
+ * @author bharat.verma
+ * @created Tuesday, 14 February 2023
+ */
+public final class SagaConstants {
+
+    private SagaConstants() {
+    }
+    public static final String ORDER_SAGA_NAME = "OrderProcessingSaga";
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/com/bharat/food/ordering/system/order/service/domain/outbox/scheduler/approval/RestaurantApprovalOutboxCleanerScheduler.java
@@ -0,0 +1,54 @@
+package com.bharat.food.ordering.system.order.service.domain.outbox.scheduler.approval;
+/*
+ * @author bharat.verma
+ * @created Tuesday, 14 February 2023
+ */
+
+
+import com.bharat.food.ordering.system.order.service.domain.outbox.model.approval.OrderApprovalOutboxMessage;
+import com.bharat.food.ordering.system.outbox.OutboxScheduler;
+import com.bharat.food.ordering.system.outbox.OutboxStatus;
+import com.bharat.food.ordering.system.saga.SagaStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class RestaurantApprovalOutboxCleanerScheduler implements OutboxScheduler {
+
+    private final ApprovalOutboxHelper approvalOutboxHelper;
+
+    public RestaurantApprovalOutboxCleanerScheduler(ApprovalOutboxHelper approvalOutboxHelper) {
+        this.approvalOutboxHelper = approvalOutboxHelper;
+    }
+
+    @Override
+    @Scheduled(cron = "@midnight")
+    public void processOutboxMessage() {
+        Optional<List<OrderApprovalOutboxMessage>> outboxMessagesResponse =
+                approvalOutboxHelper.getApprovalOutboxMessageByOutboxStatusAndSagaStatus(
+                        OutboxStatus.COMPLETED,
+                        SagaStatus.SUCCEEDED,
+                        SagaStatus.FAILED,
+                        SagaStatus.COMPENSATED);
+        if (outboxMessagesResponse.isPresent()) {
+            List<OrderApprovalOutboxMessage> outboxMessages = outboxMessagesResponse.get();
+            log.info("Received {} OrderApprovalOutboxMessage for clean-up. The payloads: {}",
+                    outboxMessages.size(),
+                    outboxMessages.stream().map(OrderApprovalOutboxMessage::getPayload)
+                            .collect(Collectors.joining("\n")));
+            approvalOutboxHelper.deleteApprovalOutboxMessageByOutboxStatusAndSagaStatus(
+                    OutboxStatus.COMPLETED,
+                    SagaStatus.SUCCEEDED,
+                    SagaStatus.FAILED,
+                    SagaStatus.COMPENSATED);
+            log.info("{} OrderApprovalOutboxMessage deleted!", outboxMessages.size());
+        }
+
+    }
+}
