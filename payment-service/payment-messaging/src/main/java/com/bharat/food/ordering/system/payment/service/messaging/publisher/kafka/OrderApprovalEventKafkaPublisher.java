acc6a3fb22f67769b64540ad648fa43702fa3484
Bharat Verma <bindian0509@gmail.com>
2023-02-19 00:09:06 +0530

    Refactoring Order Messaging module for Outbox pattern - Part 1

---
+++ payment-service/payment-messaging/src/main/java/com/bharat/food/ordering/system/payment/service/messaging/publisher/kafka/OrderApprovalEventKafkaPublisher.java
@@ -0,0 +1,72 @@
+package com.bharat.food.ordering.system.payment.service.messaging.publisher.kafka;
+/*
+ * @author bharat.verma
+ * @created Sunday, 19 February 2023
+ */
+
+
+import com.bharat.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import com.bharat.food.ordering.system.kafka.producer.KafkaMessageHelper;
+import com.bharat.food.ordering.system.outbox.OutboxStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrderApprovalEventKafkaPublisher implements RestaurantApprovalRequestMessagePublisher {
+
+    private final OrderMessagingDataMapper orderMessagingDataMapper;
+    private final KafkaProducer<String, RestaurantApprovalRequestAvroModel> kafkaProducer;
+    private final OrderServiceConfigData orderServiceConfigData;
+    private final KafkaMessageHelper kafkaMessageHelper;
+
+    public OrderApprovalEventKafkaPublisher(OrderMessagingDataMapper orderMessagingDataMapper,
+                                            KafkaProducer<String, RestaurantApprovalRequestAvroModel> kafkaProducer,
+                                            OrderServiceConfigData orderServiceConfigData,
+                                            KafkaMessageHelper kafkaMessageHelper) {
+        this.orderMessagingDataMapper = orderMessagingDataMapper;
+        this.kafkaProducer = kafkaProducer;
+        this.orderServiceConfigData = orderServiceConfigData;
+        this.kafkaMessageHelper = kafkaMessageHelper;
+    }
+
+
+    @Override
+    public void publish(OrderApprovalOutboxMessage orderApprovalOutboxMessage,
+                        BiConsumer<OrderApprovalOutboxMessage, OutboxStatus> outboxCallback) {
+        OrderApprovalEventPayload orderApprovalEventPayload =
+                kafkaMessageHelper.getOrderEventPayload(orderApprovalOutboxMessage.getPayload(),
+                        OrderApprovalEventPayload.class);
+
+        String sagaId = orderApprovalOutboxMessage.getSagaId().toString();
+
+        log.info("Received OrderApprovalOutboxMessage for order id: {} and saga id: {}",
+                orderApprovalEventPayload.getOrderId(),
+                sagaId);
+
+        try {
+            RestaurantApprovalRequestAvroModel restaurantApprovalRequestAvroModel =
+                    orderMessagingDataMapper
+                            .orderApprovalEventToRestaurantApprovalRequestAvroModel(sagaId,
+                                    orderApprovalEventPayload);
+
+            kafkaProducer.send(orderServiceConfigData.getRestaurantApprovalRequestTopicName(),
+                    sagaId,
+                    restaurantApprovalRequestAvroModel,
+                    kafkaMessageHelper.getKafkaCallback(orderServiceConfigData.getRestaurantApprovalRequestTopicName(),
+                            restaurantApprovalRequestAvroModel,
+                            orderApprovalOutboxMessage,
+                            outboxCallback,
+                            orderApprovalEventPayload.getOrderId(),
+                            "RestaurantApprovalRequestAvroModel"));
+
+            log.info("OrderApprovalEventPayload sent to kafka for order id: {} and saga id: {}",
+                    restaurantApprovalRequestAvroModel.getOrderId(), sagaId);
+        } catch (Exception e) {
+            log.error("Error while sending OrderApprovalEventPayload to kafka for order id: {} and saga id: {}," +
+                    " error: {}", orderApprovalEventPayload.getOrderId(), sagaId, e.getMessage());
+        }
+
+
+    }
+}
